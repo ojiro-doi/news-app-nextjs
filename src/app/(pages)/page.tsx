8b0be55d3f96a6dcb75cda3fe49dd74c6664d44d
ojiro-doi <sakura.0046@icloud.com>
2024-05-27 18:20:00 +0900

    add storybook

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <h1>next</h1>
+      <h1 className='text-3xl text-red-300'>next</h1>
     </main>
   );
 }
